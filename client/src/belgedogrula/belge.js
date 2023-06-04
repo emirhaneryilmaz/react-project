65e01c89accfd13f36ae89b29d4c8a854e88458d
Emirhan ERYILMAZ <emirhan_eryilmaz@hotmail.com>
2023-06-04 13:18:43 +0300

    download pdf feature

--- client/src/belgedogrula/belge.js
+++ client/src/belgedogrula/belge.js
@@ -1,35 +1,66 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import DropdownUser from '../afterLogin/dropdown'
-import { savePDF } from '@progress/kendo-react-pdf';
+import { jsPDF } from "jspdf";
+import { useAtom } from 'jotai';
+import html2canvas from 'html2canvas';
+import { userName, userTc, userId, userCity, userStreet, userNeigh } from '../store';
 
 
 export default function Belge() {
     const location = useLocation();
     const navigate = useNavigate();
+
     const user = location.state.user;
-    const id1 = location.state.id;
-    const city = location.state.city;
-    const neigh = location.state.neigh;
-    const street = location.state.street;
-    const tcKimlik = location.state.tc;
-    const name = location.state.name;
+    const id1 = location.state.id1;
+    const tcKimlik = location.state.tcKimlik;
+    let [name] = useAtom(userName);
+    const [tck] = useAtom(userTc);
+    const [id] = useAtom(userId);
+    let [city] = useAtom(userCity);
+    let [neigh] = useAtom(userNeigh);
+    let [street] = useAtom(userStreet);
 
     const buttonHandler = async () => {
-        console.log(tcKimlik + ' ' + name);
-        // const content = `${city} ${neigh} ${street}`;
-        // const dataUri = await savePDF(
-        //     <div>
-        //         <h1>Belge İçeriği</h1>
-        //         <p>{content}</p>
-        //     </div>,
-        //     { paperSize: 'A4' }
-        // );
-        // const link = document.createElement('a');
-        // link.href = dataUri;
-        // link.download = 'belge.pdf';
-        // link.click();
+
     };
+
+    function replaceTurkishCharacters(text) {
+        const turkish = ['ş', 'Ş', 'ı', 'İ', 'ğ', 'Ğ', 'ü', 'Ü', 'ö', 'Ö', 'ç', 'Ç'];
+        const english = ['s', 'S', 'i', 'I', 'g', 'G', 'u', 'U', 'o', 'O', 'c', 'C'];
+        for (let i = 0; i < turkish.length; i++) {
+            let regex = new RegExp(turkish[i], "g");
+            text = text.replace(regex, english[i]);
+        }
+        return text;
+    }
+
+    name = replaceTurkishCharacters(name);
+    city = replaceTurkishCharacters(city);
+    neigh = replaceTurkishCharacters(neigh);
+    street = replaceTurkishCharacters(street);
+
+
+    const inputRef = useRef();
+
+    const printDocument = () => {
+        console.log(inputRef.current)
+        html2canvas(inputRef.current)
+            .then((canvas) => {
+                const imgData = canvas.toDataURL('image/png');
+                // const pdf = new jsPDF();
+                // pdf.addImage(imgData, 'JPEG', 10, 10, 180, 160); 
+                // pdf.save("download.pdf");
+                const doc = new jsPDF();
+                doc.text('Belge', 10, 10);
+                doc.text(`TC: ${tck}`, 10, 20);
+                doc.text(`Name: ${name}`, 10, 30);
+                doc.text(`City: ${city}`, 10, 40);
+                doc.text(`Neighborhood: ${neigh}`, 10, 50);
+                doc.text(`Street: ${street}`, 10, 60);
+                doc.save('download.pdf');
+            });
+    }
 
     const goHome = event => {
         event.preventDefault();
@@ -95,6 +126,8 @@ export default function Belge() {
                         <section className="serviceTitleBlock"><div className="serviceDetails"><img className="agencyLogo webp" src="//cdn.e-devlet.gov.tr/themes/ankara/images/logos/64webp/edk.1.8.0.webp" alt="" width="64" height="64" /><h2><a href="/belge-dogrulama">Belge Doğrulama</a><em></em></h2></div><div className="serviceActions"><div className="share_item"><button className="share_button" id="share_button" data-match-height="serviceActs"><i className="ico-share" aria-hidden="true"></i> <span className="serviceActions_maintitle">Paylaş</span></button><div className="share_menu" id="share_menu" data-match-height="serviceActs"><a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fwww.turkiye.gov.tr/belge-dogrulama&text=Belge+Do%C4%9Frulama&via=ekapi&related=ekapi"><i className="ico-twitter" aria-hidden="true"></i><span className="serviceActions_maintitle">Twitter'da Paylaş</span></a><a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.turkiye.gov.tr/belge-dogrulama"><i className="ico-facebook" aria-hidden="true"></i><span className="serviceActions_maintitle"> Facebook'da Paylaş</span></a></div></div></div></section><aside id="serviceHelperBlock"><ul className="serviceIntroductionBlock"><li>Bu hizmet barkodlu belge sahibi kurumların işbirliği ile e-Devlet Kapısı altyapısı üzerinden sunulmaktadır.</li><li className="miniTiming">Bu işlem için yaklaşık 3 dakikanızı ayırmalısınız.</li><li className="progressStatus">Bu işlem toplam <strong>4</strong> aşamalıdır. Şu anda <strong>1</strong>. aşamadasınız.<progress value="1" max="4"></progress></li><li><ol className="progressMeter" aria-label="Hizmet Aşamaları"><li className="current">Barkod Numarası</li><li className="incomplete">Sorgulama Alanı</li><li className="incomplete">Bilgilendirme ve Onay</li><li className="final">İşlem Sonucu</li></ol></li></ul></aside>
                         <section className="serviceContainer">
 
+                           
+                            
                             <div className="richText">e-Devlet Kapısı üzerinden oluşturulan tüm barkodlu belgeleri burada doğrulayabilirsiniz.</div>
                             <form name="mainForm" action="/belge-dogrulama?submit" method="POST" className="serviceForm">
                                 <fieldset>
@@ -111,8 +144,23 @@ export default function Belge() {
                             </form>
                             <div className="formSubmitRow">
 
-                                <button className="submitButton" onClick={buttonHandler}>Göster</button>
+
+                                <button id="gosterButton" className="submitButton" onClick={buttonHandler}>Göster</button>
+
+                                <div className='informationField'>
+                                <div ref={inputRef}>
+                                    <h1>Belge</h1>
+                                    <p>TC: {tck}</p>
+                                    <p>Name: {name}</p>
+                                    <p>City: {city}</p>
+                                    <p>Neighborhood: {neigh}</p>
+                                    <p>Street: {street}</p>
+                                </div>
+                                <button className="submitButton" onClick={printDocument}>İndir</button>
                             </div>
+                            </div>
+
+
                         </section>
                     </section>
                 </div>
